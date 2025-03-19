e32f18b58763cc671e3f5f4d937fb70531a0a628
Mark Teoh <mark.teoh@hotmail.com>
2025-03-19 15:59:32 +1000

    Finished transferring everything from html to this project

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import professionalShot from './assets/professionalshot.jpg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+        <nav>
+          <h1>My Website</h1>
+          <ul>
+            <li><a href="#about">About</a></li>
+            <li><a href="#features">Experience</a></li>
+            <li><a href="#features">Diving</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </nav>
+      </header>
+      <section id="about" className="text-center p-6 bg-sky-50">
+        <h2 className="text-2xl font-bold mb-4 text-left">About Me</h2>
+        <img src={professionalShot} alt="About Me Image" className="w-48 h-48 object-cover object-top rounded-full mx-auto mb-4" />
+        <p> Hi, I am Mark, a software engineer and UQ graduate with experience as an Automation Test Engineer. Passionate about problem-solving and software development. Outside of tech, I am also a dedicated diving coach, helping athletes refine their skills and reach new heights.</p>
+      </section>
     </>
   )
 }
-
 export default App
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite'
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist',  // Output directory
-    rollupOptions: {
-      input: './index.html', // Tell Vite where to find index.html
-    },
-  },
+  plugins: [react(),tailwindcss()],
+  base: "",
 })
