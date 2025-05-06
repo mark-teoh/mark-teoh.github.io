a12095a100ec3845435b18e0a8160ee042716c3b
Mark Teoh <mark.teoh@hotmail.com>
2025-05-06 22:32:47 +1000

    Made the homepage CSS a little prettier

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,19 +4,26 @@ import Nav from '../component/nav/Nav'
 const HomePage = () => {
   return (
     <>
-    <header>
-      <Nav />
-    </header>
-    <section id="about" className="text-center p-6 bg-sky-50">
-      <h2 className="text-2xl font-bold mb-4 text-left">About Me</h2>
-      <img src="/professionalshot.jpg" alt="About Me Image" className="w-48 h-48 object-cover object-top rounded-full mx-auto mb-4" />
-      <p> Hi, I am Mark, a software engineer and UQ graduate with experience as an Automation Test Engineer. Passionate about problem-solving and software development. Outside of tech, I am also a dedicated diving coach, helping athletes refine their skills and reach new heights.</p>
-      <p> My full CV is{" "}
-        <a href="/resume.pdf"  target="_blank" rel="noopener noreferrer" className="underline text-blue-500 hover:text-blue-700">
-          here
-        </a>
-  </p>
-    </section>
+    <div className="bg-gray-100 min-h-screen">
+      <header>
+        <Nav />
+      </header>
+      <div className="flex justify-center mt-10">
+        <section id="about"className="max-w-4xl w-full bg-sky-50 p-16 rounded-xl shadow-md flex items-center gap-6"
+        >
+          <div className="flex-1 text-left text-lg space-y-4">
+            <h2 className="text-6xl font-bold mb-8 font-montserrat">About Me</h2>
+            <p className='font-inter'> Hi, I am Mark, a software engineer and UQ graduate with experience as an Automation Test Engineer. Passionate about problem-solving and software development. Outside of tech, I am also a dedicated diving coach, helping athletes refine their skills and reach new heights.</p>
+            <p className='font-inter'> My full CV is{" "}
+              <a href="/resume.pdf"  target="_blank" rel="noopener noreferrer" className="underline text-blue-500 hover:text-blue-700">
+                here
+              </a>
+            </p>
+          </div>
+          <img src="/professionalshot.jpg" alt="About Me Image" className="w-80 h-80 object-cover object-top rounded-full mx-auto mb-4" />
+        </section>
+      </div>
+    </div>
   </>
   )
 }
