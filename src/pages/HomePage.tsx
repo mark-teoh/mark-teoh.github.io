145c3c77e6d347ef890e3771e5ae950c87b651ff
Mark Teoh <mark.teoh@hotmail.com>
2025-05-06 23:10:52 +1000

    made the blue section larger and change font size

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -9,12 +9,11 @@ const HomePage = () => {
         <Nav />
       </header>
       <div className="flex justify-center mt-10">
-        <section id="about"className="max-w-4xl w-full bg-sky-50 p-16 rounded-xl shadow-md flex items-center gap-6"
-        >
+      <section id="about" className="bg-sky-50 px-16 py-16 rounded-xl shadow-md flex items-center gap-6 max-w-screen-xl w-full">
           <div className="flex-1 text-left text-lg space-y-4">
             <h2 className="text-6xl font-bold mb-8 font-montserrat">About Me</h2>
-            <p className='font-inter'> Hi, I am Mark, a software engineer and UQ graduate with experience as an Automation Test Engineer. Passionate about problem-solving and software development. Outside of tech, I am also a dedicated diving coach, helping athletes refine their skills and reach new heights.</p>
-            <p className='font-inter'> My full CV is{" "}
+            <p className='max-w-5xl mx-auto text-xl font-inter'> Hi, I am Mark, a software engineer and UQ graduate with experience as an Automation Test Engineer. Passionate about problem-solving and software development. Outside of tech, I am also a dedicated diving coach, helping athletes refine their skills and reach new heights.</p>
+            <p className='max-w-5xl mx-auto text-xl font-inter'> My full CV is{" "}
               <a href="/resume.pdf"  target="_blank" rel="noopener noreferrer" className="underline text-blue-500 hover:text-blue-700">
                 here
               </a>
